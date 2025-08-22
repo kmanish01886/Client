42d9657375dda74b6c3f9bb8a98572daa96f3146
Manish Kushwaha <kmanish274406@gmail.com>
2025-08-23 05:28:09 +0530

    tailwind and daisy ui implemented.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,42 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit,  Signal,  signal } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('client');
+export class App implements OnInit {
+  
+  private http=inject(HttpClient);
+  protected  title = 'client';
+  protected members=signal<any>([]);
+
+  // ngOnInit(): void {
+  //  this.http.get('https://localhost:44324/api/Members/AllMembers').subscribe({
+  //   // next:response=>console.log(response),
+  //   next:response=>this.members.set(response),
+  //   error:error=>console.log(error),
+  //   complete:()=>console.log('Complete the http request')
+  //  })
+  // }
+
+  async ngOnInit() {
+    this.members.set(await this.getMemebers())
+  }
+
+  async getMemebers(){
+    try{
+        return lastValueFrom(this.http.get('https://localhost:44324/api/Members/AllMembers'));
+    }
+    catch(error)
+    {
+      console.log(Error);
+      throw error;
+    }
+  }
+
+
+  
 }
