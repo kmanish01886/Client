0818e57d3dd4f584325dae0db39a6bed0f153e41
Manish Kushwaha <kmanish274406@gmail.com>
2025-09-23 07:25:53 +0530

    account service Updated.

--- src/layout/nav/nav.ts
+++ src/layout/nav/nav.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AccountService } from '../../core/services/account-service';
 
 @Component({
   selector: 'app-nav',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './nav.html',
   styleUrl: './nav.css'
 })
 export class Nav {
+  private accountservice=inject(AccountService)
+protected creds:any={}
+protected loggedIn=signal(false)
+
+login(){
+  this.accountservice.login(this.creds).subscribe({
+    next:result=>
+    {
+      console.log(result),
+      this.loggedIn.set(true);
+    }
+    //error:error=>alert(error.message)
+  })
+}
+
+logout()
+{
+  this.loggedIn.set(false);
+}
+
+
 
 }
