ff94f76924ab56e9eb55ec2caf166bab68adb929
Manish Kushwaha <kmanish274406@gmail.com>
2025-09-28 18:57:12 +0530

    end of section 6.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,13 +4,22 @@ import { MemberDetailes } from '../features/members/member-detailes/member-detai
 import { MemberList } from '../features/members/member-list/member-list';
 import { Lists } from '../features/lists/lists';
 import { Messages } from '../features/messages/messages';
+import { authGuard } from '../core/guard/auth-guard';
 
 export const routes: Routes = [
 {path:'', component:Home},
-{path:'members', component:MemberList},
-{path:'members/:id', component:MemberDetailes},
-{path:'lists', component:Lists},
-{path:'messages', component:Messages},
+{
+    path:'',
+    runGuardsAndResolvers:'always',
+    canActivate:[authGuard],
+    children:[
+        {path:'members', component:MemberList},
+        {path:'members/:id', component:MemberDetailes},
+        {path:'lists', component:Lists},
+        {path:'messages', component:Messages},
+    ]
+},
+
 {path:'**', component:Home},
 
 
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,5 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, inject, OnInit,  Signal,  signal } from '@angular/core';
-import { lastValueFrom } from 'rxjs';
+import { Component, inject } from '@angular/core';
 import { Nav } from "../layout/nav/nav";
-import { AccountService } from '../core/services/account-service';
-import { Home } from "../features/home/home";
-import { User } from '../types/user';
 import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
@@ -13,44 +8,6 @@ import { Router, RouterOutlet } from '@angular/router';
   styleUrl: './app.css',
   imports: [Nav, RouterOutlet]
 })
-export class App implements OnInit {
-  private accountservice=inject(AccountService);
-  private http=inject(HttpClient);
+export class App{
   protected router=inject(Router);
-  protected  title = 'client';
-  protected members=signal<User[]>([]);
-
-  // ngOnInit(): void {
-  //  this.http.get('https://localhost:44324/api/Members/AllMembers').subscribe({
-  //   // next:response=>console.log(response),
-  //   next:response=>this.members.set(response),
-  //   error:error=>console.log(error),
-  //   complete:()=>console.log('Complete the http request')
-  //  })
-  // }
-
-  async ngOnInit() {
-    this.members.set(await this.getMemebers());
-    this.setCurrentUser();
-  }
-
-  setCurrentUser(){
-    const userString=localStorage.getItem('user');
-    if(!userString)return;
-    const user=JSON.parse(userString);
-    this.accountservice.currentUser.set(user);
-  }
-  async getMemebers(){
-    try{
-        return lastValueFrom(this.http.get<User[]>('https://localhost:44324/api/Members/AllMembers'));
-    }
-    catch(error)
-    {
-      console.log(Error);
-      throw error;
-    }
-  }
-
-
-  
 }
