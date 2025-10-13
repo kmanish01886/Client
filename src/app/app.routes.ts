0a92100fe1400c235684ff5d623097f867683c0c
Manish Kushwaha <kmanish274406@gmail.com>
2025-10-13 07:02:06 +0530

    updated.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,6 +8,9 @@ import { authGuard } from '../core/guard/auth-guard';
 import { TestErrors } from '../features/test-errors/test-errors';
 import { NotFound } from '../shared/errors/not-found/not-found';
 import { ServerErrror } from '../shared/errors/server-errror/server-errror';
+import { MemberProfile } from '../features/members/member-profile/member-profile';
+import { MemberPhotos } from '../features/members/member-photos/member-photos';
+import { MemberMessages } from '../features/members/member-messages/member-messages';
 
 export const routes: Routes = [
 {path:'', component:Home},
@@ -17,7 +20,17 @@ export const routes: Routes = [
     canActivate:[authGuard],
     children:[
         {path:'members', component:MemberList},
-        {path:'members/:id', component:MemberDetailes},
+        {
+            path:'members/:id',
+             component:MemberDetailes,
+             children:[
+                {path:'', redirectTo:'profile',pathMatch:'full'},
+                {path:'profile', component:MemberProfile, title:'Profile'},
+                {path:'photos', component:MemberPhotos, title:'Photos'},
+                {path:'messages', component:MemberMessages, title:'Messages'}
+
+             ]
+        },
         {path:'lists', component:Lists},
         {path:'messages', component:Messages},
     ]
--- src/features/members/member-detailes/member-detailes.ts
+++ src/features/members/member-detailes/member-detailes.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
-
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { MemberService } from '../../../core/services/member-service';
+import { ActivatedRoute, NavigationEnd, Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { AsyncPipe } from '@angular/common';
+import { filter, Observable } from 'rxjs';
+import { Member } from '../../../types/member';
 @Component({
   selector: 'app-member-detailes',
-  imports: [],
+  imports: [AsyncPipe, RouterLink, RouterLinkActive, RouterOutlet],
   templateUrl: './member-detailes.html',
   styleUrl: './member-detailes.css'
 })
-export class MemberDetailes {
+export class MemberDetailes implements OnInit {
+  
+  private memberService=inject(MemberService);
+  private route=inject(ActivatedRoute);
+  private router=inject(Router);
+  protected member$?:Observable<Member>;
+  protected title=signal<string | undefined>('profile');
+
+  ngOnInit(): void {
+    this.member$=this.loadMember();
+    this.title.set(this.route.firstChild?.snapshot?.title)
+    this.router.events.pipe(
+      filter(event=>event instanceof NavigationEnd)
+    ).subscribe({
+      next:()=>{
+        this.title.set(this.route.firstChild?.snapshot?.title)
+      }
+    })
+  }
+
+  loadMember(){
+    const idParam=this.route.snapshot.paramMap.get('id');
+    if(!idParam) return;
+    const id=Number(idParam);
+    return this.memberService.getMember(id);
+  }
 
 }
